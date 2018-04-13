3be505e543057732f9856f4e6fa0dd7186c39816
Glen Max Hocky <glen842@voth28.uchicago.edu>
2018-04-13 11:56:40 -0500

    First version of coming soon page

---
+++ js/coming-soon.js
@@ -0,0 +1,14 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Vide - Video Background Settings
+  $('body').vide({
+  //  mp4: "mp4/bg.mp4",
+  //  poster: "img/bg-mobile-fallback.jpg"
+  //  poster: "img/bg-chicago-sl-2.jpg"
+    poster: "img/nyc-central-park-crop-16x9.jpg"
+  }, {
+    posterType: 'jpg'
+  });
+
+})(jQuery); // End of use strict
